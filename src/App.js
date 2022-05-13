d56be03ff373c8ef1bd71c424a1e4b011fbc7a1c
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2022-05-13 16:37:02 +0700

    authentication setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components';
+import {
+  Explore,
+  Offers,
+  SignIn,
+  SignUp,
+  ForgotPassword,
+  Profiles,
+} from './pages/';
+
 function App() {
   return (
     <>
-      <h1>My app</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   );
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import Explore from './Explore';
+import ForgotPassword from './ForgotPassword';
+import Offers from './Offers';
+import Profiles from './Profiles';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+export { Explore, ForgotPassword, Offers, Profiles, SignIn, SignUp };
